f0424fbe346b43ef0a33ab2b5e48f6bef06dc44d
Tanay2212 <tanay22198@gmail.com>
2024-04-28 11:35:04 +0530

    [REM] Removed module

--- services/models/ir_attachment.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields, models
-
-class IrAttachment(models.Model):
-    _inherit = "ir.attachment"
-
-    service_order_id = fields.Many2one("service.order")
--- services/wizards/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import service_make_invoice_advance
-from . import account_move_send
